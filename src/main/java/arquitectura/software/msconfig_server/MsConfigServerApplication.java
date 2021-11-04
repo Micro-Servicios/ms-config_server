8e9fa5305dd87fb68af9e03fccfdbbc295fd4d46
AlejoErwin <alejo.gutierrez.erwin@gmail.com>
2021-11-04 09:53:47 -0400

    se implemento el dar de baja al implementar

--- src/main/java/arquitectura/software/msconfig_server/MsConfigServerApplication.java
+++ src/main/java/arquitectura/software/msconfig_server/MsConfigServerApplication.java
@@ -2,8 +2,10 @@ package arquitectura.software.msconfig_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class MsConfigServerApplication {
 
 	public static void main(String[] args) {
